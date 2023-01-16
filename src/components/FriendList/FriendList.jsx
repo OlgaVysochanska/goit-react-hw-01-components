81ef3076e9f7db697246d96a1b88f4fe35407592
OlgaVysochanska <olavysochanska@gmail.com>
2023-01-16 14:28:45 +0200

    fix css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -24,7 +24,7 @@ FriendList.propTypes = {
     PropTypes.shape({
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-      status: bool.isRequired
+      isOnline: bool.isRequired
     })
   ),
 };
